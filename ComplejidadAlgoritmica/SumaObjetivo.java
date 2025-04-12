8e408c6be688699cea31f31f513321d86da15923
Daniel Anthony Mechan Cari <daniel.mechan@ucsm.edu.pe>
2025-04-12 14:20:57 -0500

    Modificacion de la Clase SumaObjetivo, se puso documentación y se mejoró el código para su claridad

--- ComplejidadAlgoritmica/SumaObjetivo.java
+++ ComplejidadAlgoritmica/SumaObjetivo.java
@@ -4,28 +4,28 @@ import java.util.HashSet;
 
 public class SumaObjetivo {
 
-    public boolean existeSumaObjetivo(int[] numeros, int objetivo) {
-        HashSet<Integer> vistos = new HashSet<>();
-        for (int numero : numeros) {
-            int complemento = objetivo - numero;
-            if (vistos.contains(complemento)) {
-                return true;
+    public boolean sumaObjetivo(int[] numeros, int objetivo){
+        HashSet<Integer> yaVistos = new HashSet<>();
+        for (int numero : numeros){
+            int falta = objetivo - numero;
+            if (yaVistos.contains(falta)){
+                return true; //Aca se muestra lo que suman
             }
-            vistos.add(numero);
-        }
+            yaVistos.add(numero); //Con esto se guarda
 
-        return false;
+        }
+        return false; //Sino deja de buscar y no se encuentra ninguno más
     }
 
     public static void main(String[] args) {
-        SumaObjetivo suma = new SumaObjetivo();
+        SumaObjetivo juego = new SumaObjetivo();
 
-        int[] ejemplo1 = {3, 5, 1, 7, 9};
+        int[]  numeros1 = {3, 5, 1, 7, 9};
         int objetivo1 = 10;
-        System.out.println("Ejemplo 1: " + suma.existeSumaObjetivo(ejemplo1, objetivo1)); // true
+        System.out.println("¿Dos números suman " + objetivo1 + "? " + juego.sumaObjetivo(numeros1, objetivo1));; // Salida: Verdadero
 
-        int[] ejemplo2 = {2, 4, 6};
+        int[] numeros2 = {2, 4, 6};
         int objetivo2 = 11;
-        System.out.println("Ejemplo 2: " + suma.existeSumaObjetivo(ejemplo2, objetivo2)); // false
+        System.out.println("¿Dos números suman " + objetivo2 + "? " + juego.sumaObjetivo(numeros2, objetivo2)); // Salida: Falso
     }
 }
